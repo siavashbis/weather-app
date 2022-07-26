ef7bcf42440bf8f248956a372392c923892ff25f
siavashbis <siavashbis@gmail.com>
2022-07-26 20:50:02 +0430

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import SearchMain from "./components/SearchMain";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <SearchMain />
     </div>
   );
 }
